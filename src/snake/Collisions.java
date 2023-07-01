0e85d843a284a1c13d3d54e7e8cda65a85c51e8c
william <william.montrojas@gmail.com>
2023-07-01 12:35:51 -0500

    changes in selfCollision method

--- src/snake/Collisions.java
+++ src/snake/Collisions.java
@@ -54,9 +54,27 @@ public class Collisions {
     }
 
     public static void selfCollision(Snake snake, GamePanel panel) {
+        /*
         if (!snake.isListEmpty()) {
             for (Rectangle hitboxe : snake.hitboxes) {
                 if (snake.getHitbox().intersects(hitboxe)) {
+                    snake.setSelfCollision(true);
+                    panel.gameOver = true;
+                }
+            }
+        }i had to comment this because there is an annoying bug where the 
+        snake head collides with its first body part, apparently is a bug in
+        the keys, but i added a temporary fix below. The keys bug shows itself when
+        the snake is for example going to the left side and you press left and down
+        keys at the same time and the snake moves to the opposite side, when this happened
+        the snake collided with its first body part. So the fix below adds a constraint
+        so that snake cannot collide with its first body part, obviously the bug when 
+        snake moves to opposite side by pressing the two keys mentioned previously is 
+        present yet, but the snake head no longer collide with its first body part.*/
+        
+        if (snake.hitboxes.size() > 1) {
+            for (int i = 1; i < snake.hitboxes.size() - 1; i++) {
+                if (snake.getHitbox().intersects(snake.hitboxes.get(i))) {
                     snake.setSelfCollision(true);
                     panel.gameOver = true;
                 }
